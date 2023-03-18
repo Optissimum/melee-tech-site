e9850bec3c16f2dc279d1d61e02f9c62587cdd79
James Mills <1290234+prologic@users.noreply.github.com>
2023-03-18 13:18:24 +1000

    Remove .dockerfiles (unused)

--- .dockerfiles/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-[ -n "${PUID}" ] && usermod -u "${PUID}" zs
-[ -n "${PGID}" ] && groupmod -g "${PGID}" zs
-
-printf "Configuring zs...\n"
-
-printf "Switching UID=%s and GID=%s\n" "${PUID}" "${PGID}"
-exec su-exec zs:zs "$@"
